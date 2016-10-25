cc2aff5f945446438501f2e9731d9e032be3ef6c
EremeevValery <veron.en55@gmail.com>
2016-10-25 15:57:20 +0600

    added models

--- src/apps/nvl/models.py
+++ src/apps/nvl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+class Match(models.Model):
+    home_team = models.CharField(max_length=255)
+    guest_team = models.CharField(max_length=255)
+    winner = models.CharField(max_length=255)
+    score_parties = models.CharField(max_length=255)
